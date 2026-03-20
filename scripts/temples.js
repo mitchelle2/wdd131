d8e3df1f5aa4ebbaa3ac06b1aac9027f2b6a96de
mitchelle2 <mitchellemadumere@gmail.com>
2026-03-20 03:26:04 +0100

    week02 Done

---
+++ scripts/temples.js
@@ -0,0 +1,8 @@
+// Get the current year
+const year = new Date().getFullYear();
+
+// Output the year to the footer
+document.getElementById("currentyear").innerHTML = year;
+
+// Output the last modified date
+document.getElementById("lastModified").innerHTML = document.lastModified;
